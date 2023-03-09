cc9ef4c2fda24cac20ac81cb00dce6d55048082d
stoyan-kukev <stoykukev@proton.me>
2023-03-09 15:39:27 +0200

    feat: enable images on astro@^2.1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        // Enable integrated astro Image type 
+        assets: true
+    }
+});
