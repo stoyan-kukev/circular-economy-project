2df185f59c73bac9e8d1ca0bd1ffc1ed0cefb2fc
stoyan-kukev <stoykukev@proton.me>
2023-03-10 20:59:30 +0200

    feat: added tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        // Enable integrated astro Image type 
-        assets: true
-    }
+  experimental: {
+    // Enable integrated astro Image type 
+    assets: true
+  },
+  integrations: [tailwind()]
 });
